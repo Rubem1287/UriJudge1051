74b6e24f39f033741779e595573d778b2dd1a9df
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 14:23:16 -0300

    Add files via upload

---
+++ Uri1051/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+namespace Uri1051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, imposto, porcentagem, e, e1,e2, v8, v18,v28;
+
+            imposto = 0;
+            porcentagem = 0;
+
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario <= 2000)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Isento");
+            }
+            else if(salario >= 2000.01 && salario <= 3000)
+            {
+                e = salario - 2000;
+
+                porcentagem = 8.0 / 100.0;
+                imposto = porcentagem * e;
+
+                Console.WriteLine();
+                Console.WriteLine(imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else if (salario >= 3000.01 && salario <= 4500)
+            {
+                e = salario - 2000;
+                e1 = e - 1000;
+
+                porcentagem = 8.0 / 100.0;
+                imposto = porcentagem * (e-e1);
+
+                porcentagem = 18.0 / 100.0;
+                imposto += porcentagem * e1;
+
+                Console.WriteLine();
+                Console.WriteLine(imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            else if (salario > 4500)
+            {
+                e = salario - 2000;
+                e1 = e - 1000;
+                e2 = e1 - 1500;
+                porcentagem = 8.0 / 100.0;
+                imposto = porcentagem * 1000;
+
+                porcentagem = 18.0 / 100.0;
+                imposto += porcentagem * 1500;
+
+
+
+                porcentagem = 28.0 / 100.0;
+                imposto += porcentagem * e2;
+
+                Console.WriteLine();
+                Console.WriteLine(imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
